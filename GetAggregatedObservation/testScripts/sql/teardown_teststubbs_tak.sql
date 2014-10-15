50e035a9eea5f39d832b7a398d6ba2f54407719b
Hans Thunberg <hans.thunberg@callistaenterprise.se>
2014-10-15 22:25:52 +0200

    Aggregated service implmented according to service contract

---
+++ GetAggregatedObservation/testScripts/sql/teardown_teststubbs_tak.sql
@@ -0,0 +1,77 @@
+-- Detta script tar bort all data  från TAK som behövdes för testerna
+
+SET @popularDomainName = 'Hantera hälsorelaterade tillstånd, basuppgifter - GetObservation';
+
+-- Ta bort all anropsbehörigheter
+
+DELETE FROM `Anropsbehorighet` WHERE integrationsavtal = 'Integrationstest Aggregerandetjänst';
+
+-- Ta bort logiska adresser
+DELETE FROM `LogiskAdress`
+WHERE logiskAdressat_id IN (
+ SELECT id FROM `LogiskAdressat` adressat
+ WHERE adressat.hsaId = 'HSA-ID-1'
+);
+
+DELETE FROM `LogiskAdress`
+WHERE logiskAdressat_id IN (
+ SELECT id FROM `LogiskAdressat` adressat
+ WHERE adressat.hsaId = 'HSA-ID-2'
+);
+
+DELETE FROM `LogiskAdress`
+WHERE logiskAdressat_id IN (
+ SELECT id FROM `LogiskAdressat` adressat
+ WHERE adressat.hsaId = 'HSA-ID-3'
+);
+
+DELETE FROM `LogiskAdress`
+WHERE logiskAdressat_id IN (
+ SELECT id FROM `LogiskAdressat` adressat
+ WHERE adressat.hsaId = 'HSA-ID-4'
+);
+
+DELETE FROM `LogiskAdress`
+WHERE logiskAdressat_id IN (
+ SELECT id FROM `LogiskAdressat` adressat
+ WHERE adressat.hsaId = 'HSA-ID-5'
+);
+
+DELETE FROM `LogiskAdress`
+WHERE logiskAdressat_id IN (
+ SELECT id FROM `LogiskAdressat` adressat
+ WHERE adressat.hsaId = 'HSA-ID-6'
+);
+
+DELETE FROM `LogiskAdress`
+WHERE logiskAdressat_id IN (
+ SELECT id FROM `LogiskAdressat` adressat
+ WHERE adressat.hsaId = 'HSA-ID-7'
+);
+
+-- Ta bort producent
+Delete FROM `Tjanstekomponent`
+WHERE hsaId = CONCAT('HSA-ID-TESTSTUBBE-',@popularDomainName);
+
+
+-- Ta bort logiska adressater
+DELETE FROM `LogiskAdressat`
+WHERE hsaId = 'HSA-ID-1';
+
+DELETE FROM `LogiskAdressat`
+WHERE hsaId = 'HSA-ID-2';
+
+DELETE FROM `LogiskAdressat`
+WHERE hsaId = 'HSA-ID-3';
+
+DELETE FROM `LogiskAdressat`
+WHERE hsaId = 'HSA-ID-4';
+
+DELETE FROM `LogiskAdressat`
+WHERE hsaId = 'HSA-ID-5';
+
+DELETE FROM `LogiskAdressat`
+WHERE hsaId = 'HSA-ID-6';
+
+DELETE FROM `LogiskAdressat`
+WHERE hsaId = 'HSA-ID-7';
