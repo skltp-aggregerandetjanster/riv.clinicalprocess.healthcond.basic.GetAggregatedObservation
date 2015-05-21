765f6d143c63f1b8b6d623d4e8eea8484101c441
Martin Flower <martin.flower@callistaenterprise.se>
2015-05-21 13:41:36 +0200

    SERVICE-188 - Gatling tests

---
+++ test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/description/getaggregatedobservations/scenarios/GetAggregatedObservationsScenario.scala
@@ -0,0 +1,28 @@
+package se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.description.getaggregatedobservations.scenarios
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.check.HttpCheck
+import scala.util.Random
+
+object GetAggregatedObservationsScenario {
+
+  val headers = Map(
+    "Accept-Encoding"                        -> "gzip,deflate",
+    "Content-Type"                           -> "text/xml;charset=UTF-8",
+    "SOAPAction"                             -> "urn:riv:clinicalprocess:healthcond:basic:GetObservationsResponder:1:GetObservations",
+    "x-vp-sender-id"                         -> "test",
+    "x-rivta-original-serviceconsumer-hsaid" -> "test",
+    "Keep-Alive"                             -> "115")
+
+  val request = exec(
+        http("GetAggregatedObservations ${patientid} - ${name}")
+          .post("")
+          .headers(headers)
+          .body(ELFileBody("GetObservations.xml"))
+          .check(status.is(session => session("status").as[String].toInt))
+          .check(xpath("soap:Envelope", List("soap" -> "http://schemas.xmlsoap.org/soap/envelope/")).exists)
+          .check(substring("GetObservationsResponse"))
+          .check(xpath("//ns2:observationGroup", List("ns2" -> "urn:riv:clinicalprocess:healthcond:basic:GetObservationsResponder:1")).count.is(session => session("count").as[String].toInt))
+      )
+}
