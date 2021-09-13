1a8ae4d9acf182d2761055bf3997d81caa69e36a
martul <mariia.tuliakova@soprasteria.com>
2021-09-13 14:49:56 +0200

    Anpassning för camel 3

--- GetAggregatedObservations-v1-main/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/basic/getaggregatedobservations/GOCreateRequestListTest.java
+++ GetAggregatedObservations-v1-main/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/basic/getaggregatedobservations/GOCreateRequestListTest.java
@@ -1,15 +1,15 @@
 package se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.basic.getaggregatedobservations;
 
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.junit.jupiter.api.BeforeAll;
 import riv.clinicalprocess.healthcond.basic.getobservationsresponder.v1.GetObservationsResponseType;
 import se.skltp.aggregatingservices.api.AgpServiceFactory;
 import se.skltp.aggregatingservices.tests.CreateFindContentTest;
 import se.skltp.aggregatingservices.data.TestDataGenerator;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 public class GOCreateRequestListTest extends CreateFindContentTest {
 
   private static GOAgpServiceConfiguration configuration = new GOAgpServiceConfiguration();
@@ -21,7 +21,7 @@ public class GOCreateRequestListTest extends CreateFindContentTest {
     super(testDataGenerator, agpServiceFactory, configuration);
   }
 
-  @BeforeClass
+  @BeforeAll
   public static void before() {
     configuration = new GOAgpServiceConfiguration();
     agpServiceFactory = new GOAgpServiceFactoryImpl();
